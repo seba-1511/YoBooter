5e665ba4757dc6e2f8ae9c7d32e5c80340099c72
seba-1511 <seba-1511@hotmail.com>
2013-06-24 18:23:36 +0200

    Added fastInnerHtml function to booter.utils.

--- app/scripts/booter.utils.js
+++ app/scripts/booter.utils.js
@@ -1,5 +1,16 @@
-var booter = (function() {
-
+/*
+Ideas of what to implement:
+- Function to get the image tag of glyphish icons
+- the better dialog interface form vs
+- the views loading and storing of handlebars
+- load js files
+- load css files
+- pre-load images
+- isEmail function
+- encrypt/decrypt with php on the other side
+*/
+var booter = (function () {
+	"use strict";
 	var myPrivate;
 
 	myPrivate = function (arg) {
@@ -7,10 +18,25 @@ var booter = (function() {
 	};
 
 	return {
-        
-		myPublic: function (arg) {
-			return arg;
+
+		base_url: '/',
+
+		icon: function(name, color, cssString) {
+			var img = new Image();
+			color = color || 'gray';
+			img.src = booter.base_url + '/images/icons/' + color + '/' + name + '.png';
+			img.setAttribute('style', cssString);
+			return img;
+		},
+
+		innerHTML: function(location, html) {
+			var oldContent, newContent;
+			oldContent = typeof location === "string" ? document.getElementById(location) : location;
+			newContent = oldContent.cloneNode(false);
+			newContent.innerHTML = html;
+			oldContent.parentNode.replaceChild(newContent, oldContent);
+			return newContent;
 		}
 	};
 
-})();
+}());
